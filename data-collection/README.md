132491b5ba02b32fa2e59bfbcc7a497953b6891d
serenechongtrakul <serenelc@outlook.com>
2020-02-11 16:56:03 +0000

    Added README

---
+++ data-collection/README.md
@@ -0,0 +1,4 @@
+*TO DO*
+
+- Set up a server to host the code so that I can keep the script running in the background for a minimum of a month
+- Change the API call to use the one that gives me estimated arrival times + vehicle ids.
--- data-collection/main.py
+++ data-collection/main.py
@@ -1,5 +1,6 @@
 import urllib.request
 from urllib.error import HTTPError, URLError
+from pathlib import Path
 import json 
 import time
 import datetime as dt
@@ -14,8 +15,7 @@ def main():
     high_street_ken = '490000110F'
 
     # Get all current bus arrival time information
-    bus_information = load_bus_information()
-    print(bus_information)
+    bus_information = load_bus_information(bus_9, high_street_ken)
 
     repeat_call_api(1, bus_9, high_street_ken, bus_information)
 
@@ -30,41 +30,46 @@ def repeat_call_api(num_calls: int, bus_route_id: str, bus_stop_id: str, info):
         print(dt.datetime.now())
         arrival_info = call_countdown_api(bus_route_id, bus_stop_id, info)
         
-    write_to_csv(arrival_info)
+    write_to_csv(arrival_info, bus_route_id, bus_stop_id)
 
 
-def load_bus_information():
+def load_bus_information(bus_id, station):
     bus_information = []
-
-    try:
-        with open('bus_arrivals.csv') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter = ',')
-            line_count = 0
-            for row in csv_reader:
-                if line_count != 0:
-                    vehicle_id = row[0]
-                    direction = row[1]
-                    timestamp = row[2]
-                    expected_arrival = row[3]
-                    arrived = row[4]
-                    vehicle_info = {
-                        "vehicle_id": vehicle_id,  
-                        "direction": direction,
-                        "timestamp": timestamp,
-                        "expected_arrival": expected_arrival,
-                        "arrived": arrived
-                    }
-                    bus_information.append(vehicle_info)
-                line_count += 1
-    except IOError:
-        print("I/O error in loading information from csv file")
+    # file_name = 'bus_arrivals_' + bus_id + '_' + station + '.csv'
+    file_name = 'bus_arrivals.csv'
+    bus_file = Path.cwd() / file_name
+   
+    if bus_file.is_file():
+        try:
+            with open(file_name) as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter = ',')
+                line_count = 0
+                for row in csv_reader:
+                    if line_count != 0:
+                        vehicle_id = row[0]
+                        direction = row[1]
+                        timestamp = row[2]
+                        expected_arrival = row[3]
+                        arrived = row[4]
+                        vehicle_info = {
+                            "vehicle_id": vehicle_id,  
+                            "direction": direction,
+                            "timestamp": timestamp,
+                            "expected_arrival": expected_arrival,
+                            "arrived": arrived
+                        }
+                        bus_information.append(vehicle_info)
+                    line_count += 1
+        except IOError:
+            print("I/O error in loading information from csv file")
     
     return bus_information
 
 
-def write_to_csv(arrival_array):
+def write_to_csv(arrival_array, bus_id, station):
     csv_columns = ['vehicle_id', 'direction', 'timestamp', 'expected_arrival', 'arrived']
-    csv_file = "bus_arrivals.csv"
+    # csv_file = 'bus_arrivals_' + bus_id + '_' + station + '.csv'
+    csv_file = 'bus_arrivals.csv'
     try:
         with open(csv_file, 'w') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames = csv_columns)
@@ -134,7 +139,7 @@ def vehicle_already_found(vehicle_id, dictionary):
 
 def check_if_bus_is_due(buses, info):
     now = dt.datetime.now()
-    for bus in buses:
+    for index, bus in enumerate(buses):
         eta = bus.get("expected_arrival")
         eta_as_dt = convert_time_to_datetime(eta)
         vehicle_id = bus.get("vehicle_id")
@@ -142,12 +147,33 @@ def check_if_bus_is_due(buses, info):
             print("Vehicle hasn't arrived yet: ", vehicle_id)
         else:
             print("Vehicle is due to arrive: ", vehicle_id)
-            check_if_bus_has_arrived(vehicle_id, info, bow, eta_as_dt)
+
+            # wait for 3 minutes after the bus is due to arrive
+            if eta_as_dt < now.replace(minute = now.minute - 3):
+                print("3 minutes after bus is due to arrive")
+                info = check_if_bus_has_arrived(vehicle_id, info, now, eta_as_dt, index)
     return info
 
 
-def check_if_bus_has_arrived(vehicle_id, info, time_now, time_due):
-    
+def check_if_bus_has_arrived(vehicle_id, info, time_now, time_due, index):
+    """ 
+    wait for 3 minutes after the bus is due to arrive. If the id shows back up in the 
+    API call, this implies that it hasn't arrived yet. If the id does not show back up
+    in the the API call, this implies that the bus arrived at the predicted time.
+    """
+
+    this_bus = info[index]
+    timestamp = this_bus.get("timestamp")
+    timestamp = convert_time_to_datetime(timestamp)
+
+    # check when the eta for this bus was last updated 
+    # (should only be updated if it was returned in the API call)
+    if timestamp < time_now.replace(minute = time_now.minute - 3):
+        print("Bus has arrived at predicted time")
+        this_bus["arrived"] = "True"
+        info[index] = this_bus
+
+    return info
 
 
 def convert_time_to_datetime(given_time):
