f07a4aace72e9697a15e158a4e4445cc64ea99da
serenechongtrakul <serenelc@outlook.com>
2020-04-08 22:49:33 +0700

    More postgres init

--- data-collection/init.sql
+++ data-collection/init.sql
@@ -1,7 +1,4 @@
-CREATE DATABASE busInformation;
-
-USE busInformation;
-
+-- Create valid stop tables
 CREATE TABLE valid_stop_ids_9 (stop_id VARCHAR(20), stop_name VARCHAR(50));
 CREATE TABLE valid_stop_ids_7 (stop_id VARCHAR(20), stop_name VARCHAR(50));
 CREATE TABLE valid_stop_ids_14 (stop_id VARCHAR(20), stop_name VARCHAR(50));
@@ -12,11 +9,7 @@ CREATE TABLE valid_stop_ids_37 (stop_id VARCHAR(20), stop_name VARCHAR(50));
 CREATE TABLE valid_stop_ids_35 (stop_id VARCHAR(20), stop_name VARCHAR(50));
 CREATE TABLE valid_stop_ids_69 (stop_id VARCHAR(20), stop_name VARCHAR(50));
 
--- CREATE TABLE bus_arrivals_9 (vehicle_id VARCHAR(50), );
--- CREATE TABLE mytable (myfield VARCHAR(20));
--- CREATE TABLE mytable (myfield VARCHAR(20));
--- CREATE TABLE mytable (myfield VARCHAR(20));
--- CREATE TABLE mytable (myfield VARCHAR(20));
+-- do something else
 
-
-INSERT INTO valid_stop_ids_9 VALUES ('Hello', 'serene'), ('Dolly', 'does nothing');    
+-- insert stuff if you need it
+-- INSERT INTO valid_stop_ids_9 VALUES ('Hello', 'serene'), ('Dolly', 'does nothing');    
