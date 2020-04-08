36846c615ec2c0ce17cc4707870b75feb32882f2
serenechongtrakul <serenelc@outlook.com>
2020-04-08 22:21:48 +0700

    Setting up dockerfile, docker-compose and postgres

---
+++ data-collection/init.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE busInformation;
+
+USE busInformation;
+
+CREATE TABLE valid_stop_ids_9 (stop_id VARCHAR(20), stop_name VARCHAR(50));
+CREATE TABLE valid_stop_ids_7 (stop_id VARCHAR(20), stop_name VARCHAR(50));
+CREATE TABLE valid_stop_ids_14 (stop_id VARCHAR(20), stop_name VARCHAR(50));
+CREATE TABLE valid_stop_ids_452 (stop_id VARCHAR(20), stop_name VARCHAR(50));
+CREATE TABLE valid_stop_ids_52 (stop_id VARCHAR(20), stop_name VARCHAR(50));
+CREATE TABLE valid_stop_ids_328 (stop_id VARCHAR(20), stop_name VARCHAR(50));
+CREATE TABLE valid_stop_ids_37 (stop_id VARCHAR(20), stop_name VARCHAR(50));
+CREATE TABLE valid_stop_ids_35 (stop_id VARCHAR(20), stop_name VARCHAR(50));
+CREATE TABLE valid_stop_ids_69 (stop_id VARCHAR(20), stop_name VARCHAR(50));
+
+-- CREATE TABLE bus_arrivals_9 (vehicle_id VARCHAR(50), );
+-- CREATE TABLE mytable (myfield VARCHAR(20));
+-- CREATE TABLE mytable (myfield VARCHAR(20));
+-- CREATE TABLE mytable (myfield VARCHAR(20));
+-- CREATE TABLE mytable (myfield VARCHAR(20));
+
+
+INSERT INTO valid_stop_ids_9 VALUES ('Hello', 'serene'), ('Dolly', 'does nothing');    
