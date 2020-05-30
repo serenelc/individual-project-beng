dc6d61e3f0b2228f38412549edcf152c77b81484
serenechongtrakul <serenelc@outlook.com>
2020-05-30 23:15:21 +0700

    Now need to track earliest leaving vehicle to get predicted journey time

--- data-collection/tfl_predictions.py
+++ data-collection/tfl_predictions.py
@@ -27,14 +27,107 @@ For these specific stops and routes
 
 import datetime as dt
 import time
-import os
+from pathlib import Path
+from os import path
 import json
-# import psycopg2
-from local_helper import Utilities
-# from local_data_collection import Data_Collection
+import csv
+import urllib.request
 from urllib.error import HTTPError, URLError
 
 def main():
-    helper = Utilities()
+
+    stop_info = read_csv()
+
+    s = stop_info[0]
+
+    etas = get_expected_arrival_times(s.get("stop_id"), s.get("route_id"))
+
+    earliest_bus_to_leave = evaluate_bus_data(etas)
+
+    #now find this bus in the countdown response for the destination stop.
     
 
+def read_csv():
+    stop_info = []
+    file_name = 'tfl_stop_ids.csv'
+    stop_file = Path.cwd() / file_name
+
+    if stop_file.is_file():
+        try:
+            with open(file_name) as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter = ',')
+                line_count = 0
+
+                for row in csv_reader:
+                    if line_count != 0:
+                        route_id = row[0]
+                        stop_name = row[1]
+                        stop_id = row[2]
+
+                        info = {
+                            "route_id": route_id,
+                            "stop_name": stop_name,
+                            "stop_id": stop_id
+                        }
+
+                        stop_info.append(info)
+                    line_count += 1
+        except IOError:
+            print("I/O error in loading information from csv file")
+    
+    return stop_info
+
+
+def evaluate_bus_data(bus_data):
+
+    if len(bus_data) <=1 :
+        #i.e. not buses arriving at this stop in the next 30 minutes
+        return -1
+
+    for i, bus_stop in enumerate(bus_data):
+        if i == 0:
+            continue
+
+        bus_stop_name = bus_stop[1]
+        eta = dt.datetime.fromtimestamp(int(bus_stop[6])/1000.0)
+        vehicle_id = bus_stop[5]
+
+        leave_time = {
+            "bus_stop_name": bus_stop_name,
+            "leave_time": eta,
+            "vehicle_id": vehicle_id
+        }
+
+        return leave_time
+
+
+def get_expected_arrival_times(stop_code: str, route_id: str):
+    start = time.time()
+    url =  "http://countdown.api.tfl.gov.uk/interfaces/ura/instant_V1?Stopcode2=" + stop_code + "&LineName=" + route_id + "&ReturnList=StopPointName,LineName,DestinationText,EstimatedTime,ExpireTime,VehicleID,DirectionID"
+    bus_information = []
+
+    try:
+        with urllib.request.urlopen(url) as api:
+            data = api.read().decode()
+            for line in data.splitlines():
+                line = line[1:]
+                line = line[:-1]
+                line = line.replace("\"", "")
+                line_info = list(line.split(","))
+                bus_information.append(line_info)
+
+            return bus_information
+    except (HTTPError, URLError) as error:
+        # Invalid stop code, so ignore error. 
+        print("Invalid stop code")
+        return bus_information
+    except http.client.RemoteDisconnected as disconnect:
+        # remote end closed connection without response. Try again later.
+        print("remote disconnected: ", disconnect)
+        return bus_information
+    except timeout:
+        print("timeout error when getting expected arrival times")
+    comp_time = time.time() - start
+    print("Get expected arrival times: ", comp_time)
+
+main()
